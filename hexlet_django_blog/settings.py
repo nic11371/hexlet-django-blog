16cc801c9a4b41b4741a33684c732e95a5d1d367
Nic11371 <nic11371@gmail.com>
2025-01-14 21:53:01 +0300

    env

--- hexlet_django_blog/settings.py
+++ hexlet_django_blog/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,6 +82,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'postgresql': {
+        'NAME': 'DATABASE_URL'
     }
 }
 
