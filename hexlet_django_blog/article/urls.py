014cf5ae023dcf4cce7b18062414bc19103eab90
Nic11371 <nic11371@gmail.com>
2025-01-18 18:26:44 +0300

    CRUD dlete fix route

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     #     ),
     path('', IndexView.as_view(), name='articles_index'),
     path('<int:id>/edit/', ArticleFormEditView.as_view(), name='articles_update'),
+    path('<int:id>/delete/', ArticleFormDeleteView.as_view(), name='articles_delete'),
     path('<int:id>/', ArticleView.as_view(), name='articles_detail'),
     path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
-    path('<int:id>/delete/', ArticleFormDeleteView.as_view(), name='articles_delete')
 ]
